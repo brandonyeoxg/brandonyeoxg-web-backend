c68db6e44dee5fc7aab4eed11b44086f573d6c71
brandonyeoxg <brandonyeoxg@gmai.com>
2019-04-17 17:12:24 +0800

    Add server js as the main entry point for the npm server.

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const cors = require('cors')
+const corsOptions = {
+    origin: 'http://localhost:8081',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+const db = require('./app/config/db.config.js');
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync with { force: true }');
+})
+
+require('./app/route/book.route.js')(app)
+
+var server = app.listen(8080, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("App listening at http://%s:%s", host, port)
+});
